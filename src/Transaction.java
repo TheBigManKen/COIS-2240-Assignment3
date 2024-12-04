7ff48971521ce086dcb039b3363d3a7f6c7d1759
TheBigManKen <Kenevenanyanwuocha@trentu.ca>
2024-12-03 23:16:11 -0500

    Added saveTransaction method and integrated with borrowing/returning

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -19,6 +20,7 @@ public class Transaction {
             member.borrowBook(book);
             String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails);
             return true;
         } else {
             System.out.println("The book is not available.");
@@ -32,8 +34,18 @@ public class Transaction {
             book.returnBook();
             String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails);
         } else {
             System.out.println("This book was not borrowed by the member.");
+        }
+    }
+
+    private void saveTransaction(String transactionDetails) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.txt", true))) {
+            writer.write(transactionDetails);
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Error saving transaction: " + e.getMessage());
         }
     }
 
